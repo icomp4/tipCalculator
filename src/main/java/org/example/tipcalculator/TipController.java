08532ff906a15f077eb44039145d7fdda0966167
isaiah compere <rewrittenzip@gmail.com>
2024-03-05 18:26:22 -0500

    refactor

--- src/main/java/org/example/tipcalculator/TipController.java
+++ src/main/java/org/example/tipcalculator/TipController.java
@@ -2,9 +2,7 @@ package org.example.tipcalculator;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
